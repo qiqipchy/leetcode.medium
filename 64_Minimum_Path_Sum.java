a033eb1d971e8a3350ba7ad34b5485dd2ae951d5
qiqipchy <980260193@qq.com>
2015-12-10 13:09:15 +0800

    Create 64_Minimum_Path_Sum.java

---
+++ 64_Minimum_Path_Sum.java
@@ -0,0 +1,28 @@
+package com.leetcode.twentyone;
+
+import java.util.Arrays;
+
+public class Solution {
+	public int minPathSum(int[][] grid) {
+		if (grid.length == 0)
+			return 0;
+		int[][] res = new int[grid.length][grid[0].length];
+		res[0][0] = grid[0][0];
+		for (int i = 1; i < grid.length; i++)
+			res[i][0] = res[i - 1][0] + grid[i][0];
+		for (int j = 1; j < grid[0].length; j++)
+			res[0][j] = res[0][j - 1] + grid[0][j];
+		for (int i = 1; i < grid.length; i++) {
+			for (int j = 1; j < grid[i].length; j++) {
+				res[i][j] = Math.min(res[i][j - 1], res[i - 1][j]) + grid[i][j];
+			}
+		}
+		return res[grid.length - 1][grid[0].length - 1];
+	}
+
+	public static void main(String[] args) {
+		int[][] grid = { { 0, 0, } };
+
+		System.out.println(new Solution().minPathSum(grid));
+	}
+}
