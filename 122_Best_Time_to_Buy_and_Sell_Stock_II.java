77bab88a40d186babe95ab536c48edd927af20f8
qiqipchy <980260193@qq.com>
2016-01-22 16:31:11 +0800

    Create 122_Best_Time_to_Buy_and_Sell_Stock_II.java

---
+++ 122_Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -0,0 +1,22 @@
+public class Solution {
+//把每一段都买了卖
+	public int maxProfit(int[] prices) {
+		if (prices.length == 0)
+			return 0;
+		int i = 1, min = prices[0], max = 0;
+		while (i < prices.length) {
+			if (prices[i] < prices[i - 1]) {
+				max = prices[i - 1] - min + max;
+				min = prices[i];
+			}
+			i++;
+		}
+		max = prices[i - 1] - min + max;
+		return max;
+	}
+
+	public static void main(String[] args) {
+		int[] prices = { 1, 2, 2, 3, 2, 4, 5, 6, 19, 7, 45 };
+		System.out.println(new Solution().maxProfit(prices));
+	}
+}
