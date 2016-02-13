13e358659aad0a1375f542979a24e3b3269cf807
qiqipchy <980260193@qq.com>
2016-02-13 17:40:52 +0800

    Create 136_Single_Number.java

---
+++ 136_Single_Number.java
@@ -0,0 +1,24 @@
+import java.util.Hashtable;
+import java.util.Set;
+
+public class Solution {
+	public int singleNumber(int[] nums) {
+		Hashtable<Integer, Integer> ht = new Hashtable<Integer, Integer>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (ht.containsKey(nums[i]))
+				ht.remove(nums[i]);
+			else
+				ht.put(nums[i], 1);
+		}
+		Set<Integer> keySet = ht.keySet();
+		for (Integer i : keySet)
+			return i;
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 1, 2, 2, 3, 4, 3 };
+		System.out.println(new Solution().singleNumber(nums));
+	}
+}
