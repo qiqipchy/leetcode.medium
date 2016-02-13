c6d8781659908a5f45f2de02325d2a2f32bd30db
qiqipchy <980260193@qq.com>
2016-02-13 18:00:02 +0800

    Update 260_Single_Number_III.java

--- 260_Single_Number_III.java
+++ 260_Single_Number_III.java
@@ -1,26 +1,27 @@
 import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Set;
 
 public class Solution {
 	public int[] singleNumber(int[] nums) {
-        int[] single=new int[2];
-        int cnt=0;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++)
-        {
-        	if(cnt==2) return single;
-        	if(nums[i]==nums[i+1])
-        	//注意相等的话，需要将下一个i跳过；
-        		i++;
-        	else {
-				single[cnt++]=nums[i];
-			}
-        }
-        single[cnt]=nums[nums.length-1];
-        return single;
-    }
+		Hashtable<Integer, Integer> ht = new Hashtable<Integer, Integer>();
+		int[] res = new int[2];
+
+		for (int i = 0; i < nums.length; i++) {
+			if (ht.containsKey(nums[i])) {
+				ht.remove(nums[i]);
+			} else
+				ht.put(nums[i], 1);
+		}
+		Set<Integer> keySet = ht.keySet();
+		int k = 0;
+		for (Integer i : keySet)
+			res[k++] = i;
+		return res;
+	}
+
 	public static void main(String[] args) {
-		int[] nums={1,2,2,3,3,4};
-		int[] single=new Solution().singleNumber(nums);
-		System.out.println(Arrays.toString(single));
+		int[] nums = { 1, 1, 2, 2, 3, 3, 4, 5 };
+		System.out.println(Arrays.toString(new Solution().singleNumber(nums)));
 	}
 }
